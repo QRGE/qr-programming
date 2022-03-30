3ffa528acdf5de40abc8db30b433f9a0f31bb0fb
QR <1826255833@qq.com>
2022-03-30 21:07:46 +0800

    添加 leetcode 代码的基础包

---
+++ leetcode/0-basic/src/main/java/qr/program/leetcode/basic/ListNode.java
@@ -0,0 +1,13 @@
+package qr.program.leetcode.basic;
+
+/**
+ * @author qr
+ * @date 2022/3/30 21:06
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ leetcode/24-SwapNodesInPairs/src/main/java/qr/program/leetcode/Solution.java
@@ -0,0 +1,14 @@
+package qr.program.leetcode;
+
+import qr.program.leetcode.basic.ListNode;
+
+/**
+ * @author qr
+ * @date 2022/3/30 21:01
+ */
+public class Solution {
+
+    public ListNode swapPairs(ListNode head) {
+        return null;
+    }
+}
