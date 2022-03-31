ea6f8e2517d3c00b92e48a9bf403f249de73afaf
QR <1826255833@qq.com>
2022-03-31 20:50:58 +0800

    反转 K 组链表

---
+++ leetcode/25-ReverseNodesInKGroup/src/main/java/qr/program/leetcode/Solution.java
@@ -0,0 +1,14 @@
+package qr.program.leetcode;
+
+import qr.program.leetcode.basic.ListNode;
+
+/**
+ * @author qr
+ * @date 2022/3/31 20:34
+ */
+public class Solution {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        return null;
+    }
+}
