16dcb2de9f96d1c219bbb5dfba9302d8102063fd
QR <1826255833@qq.com>
2022-03-31 22:42:41 +0800

    栈实现队列

---
+++ leetcode/232-ImplementQueueUsingStacks/src/main/java/qr/program/leetcode/MyQueue.java
@@ -0,0 +1,44 @@
+package qr.program.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author qr
+ * @date 2022/3/31 21:56
+ */
+public class MyQueue {
+
+    private final Stack<Integer> stackIn;
+    private final Stack<Integer> stackOut;
+
+    public MyQueue() {
+        stackIn = new Stack<>();
+        stackOut = new Stack<>();
+    }
+
+    public void push(int x) {
+        stackIn.push(x);
+    }
+
+    public int pop() {
+        if (stackOut.isEmpty()) {
+            while (!stackIn.isEmpty()) {
+                stackOut.push(stackIn.pop());
+            }
+        }
+        return stackOut.pop();
+    }
+
+    public int peek() {
+        if (stackOut.isEmpty()) {
+            while (!stackIn.isEmpty()) {
+                stackOut.push(stackIn.pop());
+            }
+        }
+        return stackOut.peek();
+    }
+
+    public boolean empty() {
+        return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+}
