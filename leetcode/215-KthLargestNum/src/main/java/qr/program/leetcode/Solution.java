2b9cd0248fd8eedf41357d606d780c3dd975306a
QR <1826255833@qq.com>
2022-05-07 17:37:18 +0800

    第 k 大的数

--- leetcode/215-KthLargestNum/src/main/java/qr/program/leetcode/Solution.java
+++ leetcode/215-KthLargestNum/src/main/java/qr/program/leetcode/Solution.java
@@ -8,11 +8,35 @@ import java.util.Queue;
  * @Author qr
  * @Date 2022/5/7-16:42
  */
+@SuppressWarnings("all")
 public class Solution {
 
     public static void main(String[] args) {
         int[] nums = {3,2,1,5,6,4};
         System.out.println(new Solution().findKthLargest(nums, 3));
+    }
+
+    public int findKthLargest2(int[] nums, int k) {
+        int maxIndex;
+        // 执行 k 次选择
+        for (int i = 0; i < k; i++) {
+            maxIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[maxIndex] < nums[j]) {
+                    // 记录最大值的下标
+                    maxIndex = j;
+                }
+            }
+            // 将最大元素交换至首位
+            swap(nums, i, maxIndex);
+        }
+        return nums[k - 1];
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     /**
